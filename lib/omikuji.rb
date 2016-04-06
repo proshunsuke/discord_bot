39df2752720e608f06c5ea27a5bd8e1f213983d6
proshunsuke <shunsuke0901@gmail.com>
2016-04-06 17:48:01 +0900

    くじ定数化

--- lib/omikuji.rb
+++ lib/omikuji.rb
@@ -1,4 +1,6 @@
 class Omikuji
+  KUJI = %w(大吉 中吉 小吉 吉 半吉 末吉 末小吉 凶 小凶 半凶 末凶 大凶)
+
   def self.random_select
     kuji_list.sample
   end
@@ -6,6 +8,6 @@ class Omikuji
   private
 
   def self.kuji_list
-    ['大吉', '中吉', '小吉', '吉', '半吉', '末吉', '末小吉', '凶', '小凶', '半凶', '末凶', '大凶']
+    KUJI
   end
 end
